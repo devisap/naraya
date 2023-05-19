36b7ed90de79f9b597cfc2049db5d4712dba510c
devisap <ilham.sagitaputra@gmail.com>
2023-05-20 05:31:04 +0700

    register n login api

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,27 @@ class User extends Model
 
     protected $table = 'user';
     protected $primaryKey = 'u_id';
+    
+    protected $fillable = [
+        "u_email",
+        "u_password",
+        "u_r_id",
+        "u_name",
+        "u_passpor_number",
+        "u_passpor_img",
+        "u_passpor_exp_date",
+        "u_permit_date",
+        "u_permit_exp_date",
+        "u_payment_nominal",
+        "u_payment_date",
+        "u_img",
+        "u_c_id",
+        "u_cidb",
+        "u_perkeso",
+        "u_insuran",
+        "u_number_phone",
+        "created_at"
+    ];
 
     protected $casts = [
         'updated_at' => 'datetime'
